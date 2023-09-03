d75dfbecef3b1f6f7d35a59c77a03345094635c4
Icaro Lavrador <icarolavrador@gmail.com>
2023-09-03 16:22:55 +1200

    Move Ruby around

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -1,13 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    private void Start(){
+        //QualitySettings.vSyncCount = 0;
+        //Application.targetFrameRate = 10;
+    }
+
     void Update()
     {
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+        
         var position = transform.position;
-        position.x = position.x + 0.1f;
+        position.x = position.x + 1f * horizontal * Time.deltaTime;
+        position.y = position.y + 1f * vertical * Time.deltaTime;
         transform.position = position;
     }
 }
